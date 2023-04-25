3b11a4b1ed0d3dcb92a8cb665dfb5c2326d8c36a
Swarada Bhosale <swaradapb113@gmail.com>
2023-04-25 17:51:45 +0530

    Changes for the mastersheet

--- controllers/applicantController.js
+++ controllers/applicantController.js
@@ -1,77 +1,96 @@
-const Applicant = require('../models/Applicant');
-const {StatusCodes} = require('http-status-codes')
-const customError = require('../errors');
-const catchAsync = require('../middlewares/catchAsync');
+const Applicant = require('../models/Applicant')
+const { StatusCodes } = require('http-status-codes')
+const customError = require('../errors')
+const catchAsync = require('../middlewares/catchAsync')
 
 //multer
-const multer = require('multer');  
+const multer = require('multer')
 
 const multerStorage = multer.diskStorage({
-    destination:(req,file,cb) => {
-        cb(null,'./uploads/');
-    },
-    filename:(req,file,cb) =>{
-        const ext = file.mimetype.split('/')[1];
-        cb(null,`user-${Date.now()}-${Date.now()}.${ext}`);
-    }
-});
-const multerFilter = (req,file,cb) => {
-    if(file.mimetype.startsWith('image')){
-        cb(null,true);
-    }
-    else{
-        cb(null,);
-    }
-}
-const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter
-});
-
-const uploadSettings = upload.single('image');
-
-
-
-const createTranscript = catchAsync(async(req,res) => {
-    console.log(req.body);
-    // const {
-    //     firstName,
-    //     lastName,
-    //     prn,
-    //     branch,
-    //     yos,
-    //     contactNo,
-    //     copies,
-    //     address,
-    //     marksheet,
-    //     yesOrignals,
-    // } = req.body;
-    // const fees = copies*700;
-    const applicant = await Applicant.create(req.body);
-    res.status(StatusCodes.CREATED).json({applicant});
-});
-
-const uploadFile = catchAsync(async(req,res) => {
-    const file = req.file;
-    if(!file){
-        throw new customError.BadRequestError('Please provide a image');
-    }
-    const name = req.body.name;
-    const yearSem = req.body.yearSem;
-    const applicant = await Applicant.findOne({ name });
-    if(!applicant){
-        throw new customError.BadRequestError('Please provide a name');
-    }
-    imageURL = `http://localhost:5000/static/${file.filename}`;
-    applicant.ReportDetails.push({ imageURL,yearSem })
-    await applicant.save();
-    res.status(StatusCodes.CREATED).json({ applicant })
-
-});
-
-const getAllApplicants = catchAsync(async(req,res)=>{
-    const applicants = await Applicant.find({});
-    res.status(StatusCodes.OK).json({applicants});
+  destination: (req, file, cb) => {
+    cb(null, './uploads/')
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    const ext = file.mimetype.split('/')[1]
+    cb(null, `user-${Date.now()}-${Date.now()}.${ext}`)
+  },
+})
+const masterStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/mastersheet')
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    cb(null, `user-${Date.now()}-${Date.now()}.xlsx`)
+  },
 })
 
-module.exports =  {createTranscript,uploadSettings,uploadFile,getAllApplicants}
+const upload = multer({
+  storage: multerStorage,
+})
+const uploadMaster = multer({
+  storage: masterStorage,
+})
+
+const uploadSettings = upload.single('image')
+const mastersheetSettings = uploadMaster.single('mastersheet')
+
+const createTranscript = catchAsync(async (req, res) => {
+  console.log(req.body)
+  // const {
+  //     firstName,
+  //     lastName,
+  //     prn,
+  //     branch,
+  //     yos,
+  //     contactNo,
+  //     copies,
+  //     address,
+  //     marksheet,
+  //     yesOrignals,
+  // } = req.body;
+  // const fees = copies*700;
+  const applicant = await Applicant.create(req.body)
+  res.status(StatusCodes.CREATED).json({ applicant })
+})
+
+const uploadFile = catchAsync(async (req, res) => {
+  const file = req.file
+  if (!file) {
+    throw new customError.BadRequestError('Please provide a image')
+  }
+  const name = req.body.name
+  const yearSem = req.body.yearSem
+  const applicant = await Applicant.findOne({ name })
+  if (!applicant) {
+    throw new customError.BadRequestError('Please provide a name')
+  }
+  imageURL = `http://localhost:5000/static/${file.filename}`
+  applicant.ReportDetails.push({ imageURL, yearSem })
+  await applicant.save()
+  res.status(StatusCodes.CREATED).json({ applicant })
+})
+
+const getAllApplicants = catchAsync(async (req, res) => {
+  const applicants = await Applicant.find({})
+  res.status(StatusCodes.OK).json({ applicants })
+})
+
+const createMasterSheet = catchAsync(async (req, res) => {
+  const file = req.file
+  if (!file) {
+    throw new customError.BadRequestError('Please provide a image')
+  }
+
+  res.status(StatusCodes.CREATED).json({ msg: 'Added mastersheet' })
+})
+
+module.exports = {
+  createTranscript,
+  uploadSettings,
+  uploadFile,
+  getAllApplicants,
+  createMasterSheet,
+  mastersheetSettings,
+}
